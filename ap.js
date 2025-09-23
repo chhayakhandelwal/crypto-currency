aaf522b7c87687c5044e13ac9babab35594cef8e
chhavi khandelwal <102668248+chhayakhandelwal@users.noreply.github.com>
2025-09-23 15:38:32 +0530

    Update ap.js

--- ap.js
+++ ap.js
@@ -1,44 +1,56 @@
-const form = document.querySelector('#searchForm');
-const res = document.querySelector('#tableResult');
-let upd;
 
-form.addEventListener('submit', (e) => {
+        const form = document.querySelector('#searchForm');
+const res = document.querySelector('#tableResult');
+var upd; 
+form.addEventListener('submit',(e)=>{
+
     e.preventDefault();
-    if (upd) clearTimeout(upd);
+    if(upd){
+        clearTimeout(upd);
+    }
 
     const ctype = form.elements.coinType.value;
+
     fetchPrice(ctype);
+
 });
 
-const fetchPrice = async (ctype) => {
-    try {
-        const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
-        const { price, volume, priceChange1d, name } = r.data.coin;
-        const target = 'USD';
 
-        res.innerHTML = `
-            <tr>
-                <td>Coin</td>
-                <td>${name}</td>
-            </tr>
-            <tr>
-                <td>Price</td>
-                <td>${price} ${target}</td>
-            </tr>
-            <tr>
-                <td>Volume</td>
-                <td>${volume}</td>
-            </tr>
-            <tr>
-                <td>Change (1d)</td>
-                <td>${priceChange1d}</td>
-            </tr>
-        `;
+const fetchPrice= async(ctype) =>{
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
+    console.log(r.data.coin.price);
+     const price = r.data.coin.price;
+     const volume  = r.data.coin.volume;
+     const change = r.data.coin.priceChange1d;
+     const base = r.data.coin.name;
+     const target = 'USD';
 
-        // auto-refresh after 10 sec
-        upd = setTimeout(() => fetchPrice(ctype), 10000);
-    } catch (err) {
-        res.innerHTML = `<tr><td colspan="2" style="color:red">Error: Invalid coin or API issue</td></tr>`;
-        console.error(err);
-    }
-};
+
+     res.innerHTML =`<tr style="background-color:blue; color:white; font-weight:700">
+     <td>
+         Property
+     </td>
+     <td>Value</td>
+ </tr>
+ <tr>
+     <td>
+         ${base}
+     </td>
+     <td>${price} ${target}</td>
+ </tr>
+ <tr>
+     <td>
+         Volume
+     </td>
+     <td>${volume}</td>
+ </tr>
+ <tr>
+     <td>
+         Change
+     </td>
+     <td>${change}</td>
+ </tr>`
+
+    upd = setTimeout(()=>fetchPrice(ctype),10000);
+
+}
